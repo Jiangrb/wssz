23162621bc2b3521a9d6269d930c64a79cbc2c59
jiangrb <260768796@qq.com>
2016-10-03 17:26:04 +0800

    add note.txt and clear code

--- idservice/src/main/java/services/LiquibaseServcie.java
+++ idservice/src/main/java/services/LiquibaseServcie.java
@@ -23,29 +23,8 @@ public class LiquibaseServcie {
 
 		String changeLogFilePath = "liquibase.xml";
 
-		/**
-		 * 注释部分本是想通过spring mvc 的上下文从 webApplicationContext中获取，但是一直提示报错 Cannot
-		 * load JDBC driver class 'org.postgresql.Driver'
-		 **/
-		// WebApplicationContext webApplicationContext =
-		// ContextLoader.getCurrentWebApplicationContext();
+		// 从注入的bean中获取Datasource
 		DataSource dataSource = (DataSource) WebContextUtils.getBean("dataSource");
-		// DataSource dataSource = (DataSource)
-		// webApplicationContext.getBean("dataSource");
-
-		// 所以采用了最古老的的办法读取配置文件
-
-		// String driver = ConfigUtils.getConfigByName("driver");
-		// String url = ConfigUtils.getConfigByName("url");
-		// String username = ConfigUtils.getConfigByName("username");
-		// String password = ConfigUtils.getConfigByName("password");
-		//
-		// BasicDataSource dataSource = new BasicDataSource();
-		// dataSource.setDriverClassName("org.postgresql.Driver");
-		// dataSource.setUsername(username);
-		// dataSource.setPassword(password);
-		// dataSource.setUrl(url);
-
 		if (dataSource == null) {
 			logger.error("get datebase error");
 		} else {
